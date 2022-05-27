6a23c1533e77b74959b15eb641d6ab3e95e56b67
alejoalcaraz <a.alcaraz@uniandes.edu.co>
2022-05-27 08:13:15 -0500

    fix

--- src/components/peliculas.js
+++ src/components/peliculas.js
@@ -1,62 +1,62 @@
 import React, { useState, useEffect } from "react";
 import { FormattedMessage } from "react-intl";
 import Pelicula from "./pelicula";
-import * as d3 from "https://cdn.skypack.dev/d3@7";
+// import * as d3 from "https://cdn.skypack.dev/d3@7";
 
 
-function graph(data) {
+// function graph(data) {
 
-    const canvas = d3.select("#canvas");
-
-
-    const width = 800;
-    const height = 500;
-    const margin = { top: 10, left: 100, bottom: 40, right: 10 };
-    const iwidth = width - margin.left - margin.right;
-    const iheight = height - margin.top - margin.bottom;
-
-    const svg = canvas.append("svg");
-
-    svg.attr("width", width);
-    svg.attr("height", height);
-    let g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+//     const canvas = d3.select("#canvas");
 
 
+//     const width = 800;
+//     const height = 500;
+//     const margin = { top: 10, left: 100, bottom: 40, right: 10 };
+//     const iwidth = width - margin.left - margin.right;
+//     const iheight = height - margin.top - margin.bottom;
 
-    const y = d3.scaleLinear()
+//     const svg = canvas.append("svg");
 
-        .domain([0,10000000])
-        .range([iheight, 0]);
+//     svg.attr("width", width);
+//     svg.attr("height", height);
+//     let g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+
+
+
+//     const y = d3.scaleLinear()
+
+//         .domain([0,10000000])
+//         .range([iheight, 0]);
         
 
-    const x = d3.scaleBand()
-        .domain(data.map(d => d.id))
-        .range([0, iwidth])
-        .padding(0.1);
+//     const x = d3.scaleBand()
+//         .domain(data.map(d => d.id))
+//         .range([0, iwidth])
+//         .padding(0.1);
 
 
-    const bars = g.selectAll("rect").data(data);
+//     const bars = g.selectAll("rect").data(data);
 
-    bars.enter().append("rect")
-        .attr("class", "bar")
-        .style("fill", "steelblue")
-        .attr("x", d => x(d.id))
-        .attr("y", d => y(d.views))
-        .attr("height", d =>iheight- y(d.views))
-        .attr("width", x.bandwidth())
+//     bars.enter().append("rect")
+//         .attr("class", "bar")
+//         .style("fill", "steelblue")
+//         .attr("x", d => x(d.id))
+//         .attr("y", d => y(d.views))
+//         .attr("height", d =>iheight- y(d.views))
+//         .attr("width", x.bandwidth())
 
-    g.append("g")
-        .classed("x--axis", true)
-        .call(d3.axisBottom(x))
-        .attr("transform", `translate(0, ${iheight})`);
+//     g.append("g")
+//         .classed("x--axis", true)
+//         .call(d3.axisBottom(x))
+//         .attr("transform", `translate(0, ${iheight})`);
 
-    g.append("g")
-        .classed("y--axis", true)
-        .call(d3.axisLeft(y));
+//     g.append("g")
+//         .classed("y--axis", true)
+//         .call(d3.axisLeft(y));
 
 
 
-};
+// };
 
 const Peliculas = () => {
     let url;
@@ -102,7 +102,7 @@ const Peliculas = () => {
             </div>
         );
     } else {
-        graph(data);
+        // graph(data);
         return (
             <><div id="pelis" class="col-12">
                 <table className="table table-striped">
